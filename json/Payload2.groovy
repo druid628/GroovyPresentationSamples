99cfdb24e6a66d817a1beb55fd6610a7008db452
Micah Breedlove <micah.breedlove@iostudio.com>
2012-04-30 14:12:44 -0500

    removed bunk file

--- json/Payload2.groovy
+++
@@ -1,8 +0,0 @@
-import groovy.json.*
- 
- def payload = new URL("http://github.com/api/v2/json/commits/list/grails/grails-core/master").text
-  
-  def slurper = new JsonParser()
-  def doc = slurper.parseText(payload)
-   
-   doc.commits.message.each { println it.text() }
